71d45c65b475faa2b08ab739e35468543307f6a4
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-16 09:30:35 -0700

    promise to sandbox

--- client/core/sandbox/sandbox.js
+++ client/core/sandbox/sandbox.js
@@ -2,10 +2,12 @@
 
 define(function (require) {
     var http = require('core/sandbox/http/http');
+    var promise = require('core/sandbox/promise/promise');
     var util = require('core/sandbox/util/util');
 
     return {
         http: http,
+        promise: promise,
         util: util
     };
 });
