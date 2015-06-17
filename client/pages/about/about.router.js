8ff4c4ad55fce1e5817bc879ea7759c3a6214ebb
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-17 15:06:49 -0700

    more about stuff

--- client/pages/about/about.router.js
+++ client/pages/about/about.router.js
@@ -20,6 +20,62 @@ define(function (require) {
             });
         });
 
+        app.get('/#/about/contact', function (context) {
+            require([
+                'navbar.viewmodel', 'text!components/navbar/navbar.html',
+                'about.viewmodel', 'text!pages/about/templates/contact.html'
+            ], function (NavbarViewModel, navBarTemplate, AboutViewModel, aboutTemplate) {
+                var partials = navBarTemplate.concat(aboutTemplate);
+                context.swap(sandbox.util.template(partials));
+
+                // apply ko bindings
+                ko.applyBindings(new NavbarViewModel(), document.getElementById('Navbar'));
+                ko.applyBindings(new AboutViewModel(), document.getElementById('Contact'));
+            });
+        });
+
+        app.get('/#/about/history', function (context) {
+            require([
+                'navbar.viewmodel', 'text!components/navbar/navbar.html',
+                'about.viewmodel', 'text!pages/about/templates/history.html'
+            ], function (NavbarViewModel, navBarTemplate, AboutViewModel, aboutTemplate) {
+                var partials = navBarTemplate.concat(aboutTemplate);
+                context.swap(sandbox.util.template(partials));
+
+                // apply ko bindings
+                ko.applyBindings(new NavbarViewModel(), document.getElementById('Navbar'));
+                ko.applyBindings(new AboutViewModel(), document.getElementById('History'));
+            });
+        });
+
+        app.get('/#/about/links', function (context) {
+            require([
+                'navbar.viewmodel', 'text!components/navbar/navbar.html',
+                'about.viewmodel', 'text!pages/about/templates/links.html'
+            ], function (NavbarViewModel, navBarTemplate, AboutViewModel, aboutTemplate) {
+                var partials = navBarTemplate.concat(aboutTemplate);
+                context.swap(sandbox.util.template(partials));
+
+                // apply ko bindings
+                ko.applyBindings(new NavbarViewModel(), document.getElementById('Navbar'));
+                ko.applyBindings(new AboutViewModel(), document.getElementById('AffiliateLinks'));
+            });
+        });
+
+        app.get('/#/about/rush', function (context) {
+            require([
+                'navbar.viewmodel', 'text!components/navbar/navbar.html',
+                'about.viewmodel', 'text!pages/about/templates/rush.html'
+            ], function (NavbarViewModel, navBarTemplate, AboutViewModel, aboutTemplate) {
+                var partials = navBarTemplate.concat(aboutTemplate);
+                context.swap(sandbox.util.template(partials));
+
+                // apply ko bindings
+                ko.applyBindings(new NavbarViewModel(), document.getElementById('Navbar'));
+                ko.applyBindings(new AboutViewModel(), document.getElementById('Rush'));
+            });
+        });
+
     };
 
     return aboutRouter;
