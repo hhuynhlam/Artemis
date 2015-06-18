3fb41f60200c00d5c42c771ab99d17e25d8826b1
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-18 14:45:33 -0700

    redirect fix

--- client/app.js
+++ client/app.js
@@ -24,7 +24,7 @@ define(function (require) {
 
 	// 404 Error
 	app.notFound = function () {
-		window.location.replace('/#/');
+		window.location.replace( window.env.CLIENT_HOST + '/' );
 	};
 
 	// Override this function so that Sammy doesn't mess with forms
