ab5c9dce78ccb44ad25612c7180bd3b4edbb440b
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-23 08:55:51 -0700

    calendar loading events

--- client/pages/event/event-calendar.viewmodel.js
+++ client/pages/event/event-calendar.viewmodel.js
@@ -4,27 +4,23 @@ define(function (require) {
     var $ = require('jquery');
     var ko = require('knockout');
     var sandbox = require('sandbox');
-    require('k/kendo.calendar.min');
+    require('fullcalendar');
 
     var EventCalendarViewModel = function () {
+        var month = sandbox.date.getStartEndOfMonth(sandbox.date.getDate().format('M'));
         this.$selector = $('#Calendar');
-        this.events = ko.observableArray([]);
 
-        // init calendar
-        var data = {
-            value: 'Hello'
-        };
+        // setup
+        this.setupObservables();
 
-        this.$selector.kendoCalendar({
-            data: data,
-            month: {
-                content: '<span>#= data.value #</span>'
-            }
-        });
-
-        this.getEvents()
+        // load events
+        this.getEvents({
+            startDate: month.start,
+            endDate: month.end
+        })
         .then(function (events) {
             this.events(events);
+            this.renderCalendar();
         }.bind(this))
         .catch(function (err) {
             console.error('Error: Cannot get events (', err, ')');
@@ -40,11 +36,48 @@ define(function (require) {
         data = {
             apiKey: window.env.API_KEY,
             event_code: options.type,
-            startDate: (options.startDate) ? sandbox.date.toUnix(options.startDate) : undefined,
-            endDate: (options.startDate) ? sandbox.date.toUnix(options.endDate) : undefined
+            startDate: (options.startDate) ? options.startDate.unix() : undefined,
+            endDate: (options.endDate) ? options.endDate.unix() : undefined
         };
 
         return sandbox.http.get(url, data);
+    };
+
+    EventCalendarViewModel.prototype.renderCalendar = function () {
+        this.$selector.fullCalendar({
+            header: {
+                left: 'prevYear prev',
+                center: 'title',
+                right: 'next nextYear'
+            },
+            
+            theme: true,
+            themeButtonIcons: {
+                prev: 'custom fa fa-angle-left',
+                next: 'custom fa fa-angle-right',
+                prevYear: 'custom fa fa-angle-double-left',
+                nextYear: 'custom fa fa-angle-double-right'
+            },
+
+            events: this.calendarEvents()
+        });
+    };
+
+    EventCalendarViewModel.prototype.setupObservables = function () {
+        this.events = ko.observableArray([]);
+        
+        this.calendarEvents = ko.computed(function () {
+            var data = [];
+            this.events().forEach(function (event) {
+                data.push({
+                    title: event.name,
+                    start: sandbox.date.parseUnix(event.date).format('YYYY-MM-DD')
+                });
+            });
+            return data;
+        }, this);
+
+        this.calendarEvents.subscribe(function () { this.renderCalendar(); }, this);
     };
 
     return EventCalendarViewModel;
