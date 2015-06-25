421def424f8e9da1b3aa841792d72b281fd267b9
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-24 19:39:38 -0700

    grid checkpoint

--- client/pages/member/member.viewmodel.js
+++ client/pages/member/member.viewmodel.js
@@ -4,6 +4,7 @@ define(function (require) {
 	var $ = require('jquery');
 	var ko = require('knockout');
 	var sandbox = require('sandbox');
+	require('k/kendo.grid.min');
 
 	var MemberViewModel = function () {
 		this.actives = ko.observableArray([]);
@@ -16,12 +17,53 @@ define(function (require) {
 		// init events
 		this.getMembers()
 		.then(function (members) {
+			var data = [];
+
+			members.forEach(function (m) {
+				// if(m.position & sandbox.constant.role.ACTIVE) { this.actives.push(m); }
+				// else if(m.position & sandbox.constant.role.ALUMNUS) { this.alumni.push(m); }
+				// else if(m.position & sandbox.constant.role.AFFILIATE) { this.affiliates.push(m); }
+				data.push({
+					Name: m.first_name + ' ' + m.last_name,
+					Class: m.class_name,
+					Family: m.family,
+					Position: m.position,
+					Phone: m.phone,
+					Email: m.email
+				});	
+
+			}, this);
 			
-			// members.forEach(function (m) {
-			// 	if(m.position & sandbox.constant.role.ACTIVE) { this.actives.push(m); }
-			// 	else if(m.position & sandbox.constant.role.ALUMNUS) { this.alumni.push(m); }
-			// 	else if(m.position & sandbox.constant.role.AFFILIATE) { this.affiliates.push(m); }
-			// }, this);
+			$selector.kendoGrid({
+				dataSource: {
+                    data: data,
+                    schema: {
+                        model: {
+                            fields: {
+                                Name: { type: "string" },
+                                Class: { type: "string" },
+                                Family: { type: "string" },
+                                Position: { type: "number" },
+                                Phone: { type: "string" },
+                                Email: { type: "string" },
+
+                            }
+                        }
+                    },
+                    pageSize: 20
+                },
+                scrollable: true,
+                sortable: true,
+                filterable: true,
+                columns: [
+                    'Name',
+	                'Class',
+	                'Family',
+	                'Position',
+	                'Phone',
+	                'Email'
+                ]
+			});
 
 		}.bind(this))
 		.catch(function (err) {
