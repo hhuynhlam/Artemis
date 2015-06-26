08e1686cad66929d30d5699b39769e0c7fd68c4b
Hai Huynhlam <hhuynhlam@gmail.com>
2015-06-26 02:36:02 -0700

    login

---
+++ server_v2/routes/login.php
@@ -0,0 +1,23 @@
+<?php
+
+$app->get('/login', function () use ($app) {
+
+    // check required params
+    if(is_null($app->request->get('username')) || is_null($app->request->get('password'))) {
+        $app->status(406); 
+        echo json_encode('You need to send a username and password.');
+        return; 
+    }
+    
+    // get request parameters
+    $username = $app->request->get('username');
+    $password = $app->request->get('password');
+
+    // construct query
+    $member = MembersQuery::create()
+        ->filterByUsername($username)
+        ->filterByPassword($password);
+
+    // execute and return
+    returnDataJSON($member->find()->toJSON(), 'Memberss');
+});
