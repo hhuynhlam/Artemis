78760e988d6e0676f7d05f6385efa9e3f9bc58b3
Hai Huynhlam <hhuynhla@advent.com>
2015-07-24 13:57:03 -0700

    hopscotch styles

--- client/services/tour/tour.js
+++ client/services/tour/tour.js
@@ -1,6 +1,7 @@
 'use strict';
 
 define(function (require) {
+    var $ = require('jquery');
     var hopscotch = require('hopscotch');
 
     var pages = {
@@ -10,6 +11,10 @@ define(function (require) {
     var tutorial = {
         start: function (page) {
             var tour = pages[page];
+            tour.onShow = function () {
+                $('.hopscotch-next').addClass('btn').addClass('btn-success');
+                $('.hopscotch-prev').addClass('btn').addClass('btn-default');
+            };
             tour.onEnd = function () { hopscotch.endTour(); };
 
             hopscotch.startTour(pages[page], 0);
