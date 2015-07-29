6fcf5cdbf087838d76b60444f58d16c1e91cd500
Hai Huynhlam <hhuynhla@advent.com>
2015-07-28 19:25:59 -0700

    hide dashboard

--- client/pages/home/home.router.js
+++ client/pages/home/home.router.js
@@ -10,7 +10,7 @@ define(function (require) {
     var homeRouter = function (app) {   
         
         app.get('/#/', function (context) {
-            if(auth.isLoggedIn()) { window.location.replace(window.env.CLIENT_HOST + '/dashboard'); return; }
+            // if(auth.isLoggedIn()) { window.location.replace(window.env.CLIENT_HOST + '/dashboard'); return; }
             require(['text!pages/home/home.html'], function (template) {
                 context.swap(sandbox.util.template(template));
 
